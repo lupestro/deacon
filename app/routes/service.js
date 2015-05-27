12f6d5f0377b006542403e08cf5bc3105295681d
Ralph Mack <lupestro@macksoft.com>
2015-05-27 12:52:41 -0400

    Made code that introduces pattern easier to read

--- app/routes/service.js
+++ app/routes/service.js
@@ -3,7 +3,29 @@ import ServiceModel from '../models/service';
 
 export default Ember.Route.extend({
 	model: function() {
-		return ServiceModel.create({ pattern:[ [7,0,1,5,6],[6,"*"],[5,2,3,4],[4,0,1],[3,"*"], [2,"*"] ]});
+		/*var pattern = [
+			[12,"*"],
+			[11,"*"],
+			[10,"*"],
+			[9,"*"],
+			[8,"*"], 
+			[7,0,1,5,6],
+			[6,2,3,4,5],
+			[5,"*"],
+			[4,2,3],
+			[4,0,1], 
+			[3,2],
+			[3,0,1] 
+		];*/
+		var pattern = [
+			[7,0,1,5,6],
+			[6,"*"],
+			[5,2,3,4],
+			[4,0,1],
+			[3,"*"],
+			[2,"*"]
+		];
+		return ServiceModel.create({ pattern: pattern});
 		//return ServiceModel.create();
 	}
 });
