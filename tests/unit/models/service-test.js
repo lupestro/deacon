6fdf26299f86c82931363754496d108a5e71dc7f
Ralph Mack <lupestro@macksoft.com>
2018-02-19 16:48:15 -0500

    Now (barely) testing in new regime

--- tests/unit/models/service-test.js
+++ tests/unit/models/service-test.js
@@ -1,12 +1,29 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import ServiceModel from 'deacon/models/service';
 
-moduleFor('model:service', 'Unit | Model | service', {
-  // Specify the other units that are required for this test.
-  needs: []
-});
+//import { run } from '@ember/runloop';
 
-test('it exists', function(assert) {
-  var model = this.subject();
-  // var store = this.store();
-  assert.ok(!!model);
+module('Unit | Model | service', function(hooks) {
+  setupTest(hooks);
+
+  test('dimensions are sane', function(assert) {
+    let model = ServiceModel.create({pattern: [[2,'*'],[2,'*'],[2,'*']]});
+    assert.ok(model.height > 100);
+    assert.ok(model.width > 100);
+  });
+  test('counts are sane', function(assert) {
+    let model = ServiceModel.create({pattern: [[2,'*'],[3,'*'],[1,'*']]});
+    assert.ok(model.plates.length === 2, 'Right number of plates');
+    assert.ok(model.deacons.length === 2, 'Right number of deacons');
+  });
+  test('seats are sane', function(assert) {
+    let model = ServiceModel.create({pattern: [[2,'*'],[3,'*'],[1,'*']]});
+    assert.ok(model.getPewSeats(-1) === 0, 'Bogus index handled correctly');
+    assert.ok(model.getPewSeats(0) === 2,'First row is right');
+    assert.ok(model.getPewSeats(1) === 3, 'Second row is right');
+    assert.ok(model.getPewSeats(2) === 1, 'Third row is right');
+    assert.ok(model.getPewSeats(3) === 0, 'Overshot index handled correctly');
+  });
+
 });
