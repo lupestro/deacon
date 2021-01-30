293f839cf4a43607af8fd7eacbe8e3dd51b52d00
Lupestro <lupestro@macksoft.com>
2021-01-30 17:29:37 -0500

    Bump deps and clean up audits

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,5 @@
 // Types for compiled templates
-declare module 'deacon/templates/*' { 
+declare module 'deacon/templates/*' {
   import { TemplateFactory } from 'htmlbars-inline-precompile';
   const tmpl: TemplateFactory;
   export default tmpl;
